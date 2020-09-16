64a69c1a05aa0605682d563eb1a382ddf11d527c
djmakwana797 <dmakwana979@rku.ac.in>
2020-09-16 22:41:38 +0530

    Users list added in WelcomeActivity.

--- app/src/main/java/com/rku/tutorial08/WelcomeActivity.java
+++ app/src/main/java/com/rku/tutorial08/WelcomeActivity.java
@@ -8,12 +8,20 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    DatabaseHelper helper;
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
+    ListView listView;
+    String UserData[] = {"TEST 1","TEST 2"};
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,23 @@ public class WelcomeActivity extends AppCompatActivity {
 
         preferences = getSharedPreferences("login",MODE_PRIVATE);
         editor = preferences.edit();
+        helper = new DatabaseHelper(this);
 
+        listView = findViewById(R.id.userDataList);
+        adapter = new ArrayAdapter<String>(
+                WelcomeActivity.this,
+                android.R.layout.simple_list_item_1,
+                helper.getUserList());
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String listItem = ((TextView)view).getText().toString();
+                Intent intent = new Intent(WelcomeActivity.this,UserDetailActivity.class);
+                intent.putExtra("username",listItem);
+                startActivity(intent);
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
